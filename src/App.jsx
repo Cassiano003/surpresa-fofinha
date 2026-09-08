759df431364ac90455eeb3d15719b8cf9187a488
Cassiano003 <cassiano.jfsantos@gmail.com>
2026-09-08 20:57:31 -0300

    Site finalmente pega

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 import { useState, useRef } from 'react';
 import './App.css';
 import './index.css';
@@ -37,7 +37,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
+    <HashRouter>
      <div className="floating-hearts">
         <span className="heart">❤️</span>
         <span className="heart">💖</span>
@@ -58,7 +58,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/surpresa" element={<Surpresa />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
