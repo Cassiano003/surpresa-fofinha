6092737c43745de4fd93b0c5775e02a670cc94b5
Cassiano003 <cassiano.jfsantos@gmail.com>
2026-09-13 15:06:15 -0300

    Polaróide maior

--- src/Surpresa.jsx
+++ src/Surpresa.jsx
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom';
 import LoveTimer from './LoveTimer';
 
 const fotos = [
-  { src: './fotos/foto1.jpg', legenda: 'Um dia qualquer, do nosso jeito', rotacao: -6 },
-  { src: './fotos/foto2.jpg', legenda: 'Aquele sorriso que eu amo', rotacao: 4 },
-  { src: './fotos/foto3.jpg', legenda: 'Sempre juntinhos', rotacao: -3 },
-  { src: './fotos/foto4.jpg', legenda: 'Um momento pra guardar', rotacao: 5 },
-  { src: './fotos/foto5.jpg', legenda: '3 meses e contando 💕', rotacao: -5 },
+
+  { src: './fotos/foto19.jpg', legenda: 'Por mais acampamentos com você ⛺', rotacao: -6 },
+  { src: './fotos/foto2.jpg', legenda: 'Chocomenta ebaa 🍦', rotacao: 4 },
+  { src: './fotos/foto3.jpeg', legenda: 'Acho que virei uma diva 😝 (mas sua alegria foi a melhor parte)', rotacao: -3 },
+  { src: './fotos/foto7.jpg', legenda: 'Realmente divei 💋', rotacao: 5 },
+  { src: './fotos/foto4.jpg', legenda: 'Feliz 2 meses de namoro 💞', rotacao: -4 },
+  { src: './fotos/foto8.jpg', legenda: 'Filminho 😃', rotacao: 6 },
+  { src: './fotos/foto11.jpg', legenda: 'Muito bom tirar foto nesse espelho da Disney 🤌', rotacao: -4 },
+  { src: './fotos/foto6.jpg', legenda: 'Ursinhos fofinhos 🐻', rotacao: 3 },
+  { src: './fotos/foto10.jpg', legenda: 'Miau! 🐱', rotacao: -5 },
+  { src: './fotos/foto17.jpg', legenda: 'Arrrgh! 🤪', rotacao: 4 },
+  { src: './fotos/foto1.jpg', legenda: 'Eu facilmente moraria nesse dia ❣️', rotacao: -6 },
+  { src: './fotos/foto14.jpg', legenda: 'O que eu mais gosto de fazer: te beijar...', rotacao: 4 },
+  { src: './fotos/foto15.jpg', legenda: 'Meu lugar favorito: seu abraço...', rotacao: -3 },
+  { src: './fotos/foto20.jpg', legenda: '3 meses e contando 💕', rotacao: 4 },
 ];
 
 function Surpresa() {
@@ -72,7 +82,7 @@ function Surpresa() {
 
         <div className={`textoFofinho ${cartaAberta ? 'aberta' : ''}`}>
           <p>
-            &nbsp;&nbsp;&nbsp;São 3 meses em que eu posso te chamar de minha namorada. São 90 dias em que meu amor por você só cresceu cada dia mais. 2160 horas em que acordo pensando em você, durmo pensando em você, vivo pensando em você...<br/><br/>...Quando menos imaginei, te dizer "eu te amo" se tornou algo tão comum. Poder te abraçar, beijar, saber que o que eu sinto é recíproco. Não tenho palavras para descrever o amor que sinto por você. A cada dia aprendo a ser alguém melhor por você...<br/><br/>...Eu te amo...<br/><br/>...Feliz 3 meses de namoro... 😝
+            &nbsp;&nbsp;&nbsp;São 3 meses em que eu posso te chamar de minha namorada. São 90 dias em que meu amor por você só cresce cada dia mais. São 2.160 horas em que acordo pensando em você, durmo pensando em você, vivo pensando em você...<br/><br/>...Quando menos imaginei, te dizer "eu te amo" se tornou algo tão comum. Poder te abraçar, beijar, saber que o que eu sinto é recíproco. Não tenho palavras para descrever o amor que sinto por você. A cada dia aprendo a ser alguém melhor por você...<br/><br/>...Eu te amo...<br/><br/>...Feliz 3 meses de namoro... 💓
           </p>
         </div>
       </div>
